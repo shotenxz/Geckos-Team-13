d7602fd2a8df725d51cb9f3a83f164111aded1c5
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-12-07 07:50:14 -0700

    Working Express server

--- app.js
+++ app.js
@@ -1,141 +1,25 @@
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-// var session = require('express-session');
-// var passport = require('passport');
-// var Auth0Strategy = require('passport-auth0');
-// var flash = require('connect-flash');
-// var userInViews = require('./lib/middleware/userInViews');
-// require('dotenv').config()
+/** EasyAuction Express App **/
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-// var authRouter = require('./routes/auth');
-
-
-// // Configure Passport to use Auth0
-// var strategy = new Auth0Strategy(
-//   {
-//     domain: process.env.AUTH0_DOMAIN,
-//     clientID: process.env.AUTH0_CLIENT_ID,
-//     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-//     callbackURL:
-//       process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
-//   },
-//   function (accessToken, refreshToken, extraParams, profile, done) {
-//     // accessToken is the token to call Auth0 API (not needed in the most cases)
-//     // extraParams.id_token has the JSON Web Token
-//     // profile has all the information from the user
-//     return done(null, profile);
-//   }
-// );
-
-// passport.use(strategy);
-// passport.serializeUser(function (user, done) {
-//   done(null, user);
-// });
-// passport.deserializeUser(function (user, done) {
-//   done(null, user);
-// });
-
-// var app = express();
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // express-session (we'll need to change the secret in prod)
-// var sess = {
-//   secret: 'SomeSecretWord',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true
-// };
-// if (app.get('env') === 'production') {
-//   sess.cookie.secure = true; // serve secure cookies, requires https
-// }
-// app.use(session(sess));
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.use(flash());
-
-// // Handle auth failure error messages
-// app.use(function (req, res, next) {
-//   if (req && req.query && req.query.error) {
-//     req.flash('error', req.query.error);
-//   }
-//   if (req && req.query && req.query.error_description) {
-//     req.flash('error_description', req.query.error_description);
-//   }
-//   next();
-// });
-
-// // Setup routes
-// app.use(userInViews());
-// app.use('/', indexRouter);
-// app.use('/', usersRouter);
-// app.use('/auth', authRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-//   console.log(req);
-// });
-
-// // Error handlers
-
-// // Development error handler
-// // Will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function (err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-
-// // Production error handler
-// // No stacktraces leaked to user
-// app.use(function (err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
-
-// // Setup mongoose here
-
-
-// module.exports = app;
-
-
+// createError was replaced by the error handler at the end of app.js
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-var dotenv = require('dotenv');
 var passport = require('passport');
 var Auth0Strategy = require('passport-auth0');
 var flash = require('connect-flash');
+// Load ENV variables
+require('dotenv').config()
+
+// Import route files
 var userInViews = require('./lib/middleware/userInViews');
 var authRouter = require('./routes/auth');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-dotenv.load();
-
-// Configure Passport to use Auth0
+// Configure Passport and create a new Auth0 instance
 var strategy = new Auth0Strategy(
   {
     domain: process.env.AUTH0_DOMAIN,
@@ -153,12 +37,9 @@ var strategy = new Auth0Strategy(
 );
 
 passport.use(strategy);
-
-// You can use this section to keep a smaller payload
 passport.serializeUser(function (user, done) {
   done(null, user);
 });
-
 passport.deserializeUser(function (user, done) {
   done(null, user);
 });
@@ -167,12 +48,12 @@ const app = express();
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'jade');  // Can replace with pug
 
 app.use(logger('dev'));
 app.use(cookieParser());
 
-// config express-session
+// express-session setup
 var sess = {
   secret: 'CHANGE THIS SECRET',
   cookie: {},
@@ -184,12 +65,11 @@ if (app.get('env') === 'production') {
   sess.cookie.secure = true; // serve secure cookies, requires https
 }
 
+// Init session middleware
 app.use(session(sess));
-
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(flash());
 
 // Handle auth failure error messages
@@ -203,6 +83,7 @@ app.use(function (req, res, next) {
   next();
 });
 
+// root routes (can remove index, once we get react stood up)
 app.use(userInViews());
 app.use('/', authRouter);
 app.use('/', indexRouter);
@@ -215,8 +96,7 @@ app.use(function (req, res, next) {
   next(err);
 });
 
-// Error handlers
-
+/*** Error handlers ***/
 // Development error handler
 // Will print stacktrace
 if (app.get('env') === 'development') {
@@ -228,7 +108,6 @@ if (app.get('env') === 'development') {
     });
   });
 }
-
 // Production error handler
 // No stacktraces leaked to user
 app.use(function (err, req, res, next) {
@@ -238,5 +117,7 @@ app.use(function (err, req, res, next) {
     error: {}
   });
 });
+
+// Initialize Mongoose here
 
 module.exports = app;
