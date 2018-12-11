def087d876d6d68c330105ef983ef0027d3a3cc6
Blarz <info.redpaz@gmail.com>
2018-12-10 18:00:31 -0600

    Fix merge issue - master and develop

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 /** EasyAuction Express App **/
 
 // createError was replaced by the error handler at the end of app.js
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var passport = require('passport');
-var Auth0Strategy = require('passport-auth0');
-var flash = require('connect-flash');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var passport = require("passport");
+var Auth0Strategy = require("passport-auth0");
+var flash = require("connect-flash");
 // Load ENV variables
-require('dotenv').config()
+require("dotenv").config();
 
 // Import route files
-var userInViews = require('./lib/middleware/userInViews');
-var authRouter = require('./routes/auth');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userInViews = require("./lib/middleware/userInViews");
+var authRouter = require("./routes/auth");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 // Configure Passport and create a new Auth0 instance
 var strategy = new Auth0Strategy(
@@ -26,9 +26,9 @@ var strategy = new Auth0Strategy(
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+      process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
   },
-  function (accessToken, refreshToken, extraParams, profile, done) {
+  function(accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
     // extraParams.id_token has the JSON Web Token
     // profile has all the information from the user
@@ -37,31 +37,31 @@ var strategy = new Auth0Strategy(
 );
 
 passport.use(strategy);
-passport.serializeUser(function (user, done) {
+passport.serializeUser(function(user, done) {
   done(null, user);
 });
-passport.deserializeUser(function (user, done) {
+passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
 const app = express();
 
 // View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');  // Can replace with pug
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade"); // Can replace with pug
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cookieParser());
 
 // express-session setup
 var sess = {
-  secret: 'CHANGE THIS SECRET',
+  secret: "CHANGE THIS SECRET",
   cookie: {},
   resave: false,
   saveUninitialized: true
 };
 
-if (app.get('env') === 'production') {
+if (app.get("env") === "production") {
   sess.cookie.secure = true; // serve secure cookies, requires https
 }
 
@@ -69,29 +69,29 @@ if (app.get('env') === 'production') {
 app.use(session(sess));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(flash());
 
 // Handle auth failure error messages
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   if (req && req.query && req.query.error) {
-    req.flash('error', req.query.error);
+    req.flash("error", req.query.error);
   }
   if (req && req.query && req.query.error_description) {
-    req.flash('error_description', req.query.error_description);
+    req.flash("error_description", req.query.error_description);
   }
   next();
 });
 
 // root routes (can remove index, once we get react stood up)
 app.use(userInViews());
-app.use('/', authRouter);
-app.use('/', indexRouter);
-app.use('/', usersRouter);
+app.use("/", authRouter);
+app.use("/", indexRouter);
+app.use("/", usersRouter);
 
 // Catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  const err = new Error('Not Found');
+app.use(function(req, res, next) {
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -99,10 +99,10 @@ app.use(function (req, res, next) {
 /*** Error handlers ***/
 // Development error handler
 // Will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
+if (app.get("env") === "development") {
+  app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
       message: err.message,
       error: err
     });
@@ -110,9 +110,9 @@ if (app.get('env') === 'development') {
 }
 // Production error handler
 // No stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: {}
   });
